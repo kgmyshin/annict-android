e6a419405ce5e9d0e75152585f323c1b884e82d7
kgmyshin <kgmyshin82@gmail.com>
2017-09-12 19:46:24 +0900

    read propetires from secrets.properties

--- modules/auth/src/main/kotlin/com/kgmyshin/auth/infra/api/json/PublishTokenRequestJson.kt
+++ modules/auth/src/main/kotlin/com/kgmyshin/auth/infra/api/json/PublishTokenRequestJson.kt
@@ -1,10 +1,11 @@
 package com.kgmyshin.auth.infra.api.json
 
 import com.google.gson.annotations.SerializedName
+import com.kgmyshin.auth.BuildConfig
 
 internal data class PublishTokenRequestJson(
-        @SerializedName("client_id") private val clientId: String,
-        @SerializedName("client_secret") private val clientSecret: String,
+        @SerializedName("client_id") private val clientId: String = BuildConfig.ANNICT_CLIENT_ID,
+        @SerializedName("client_secret") private val clientSecret: String = BuildConfig.ANNICT_CLIENT_SECRET,
         @SerializedName("grant_type") private val grantType: String = "redirect_uri",
         @SerializedName("redirect_uri") private val redirectUri: String = "urn:ietf:wg:oauth:2.0:oob",
         @SerializedName("code") private val code: String
