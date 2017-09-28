979d871a68090879900b29ed3565c3cb4e371cc3
kgmyshin <kgmyshin82@gmail.com>
2017-09-28 14:27:58 +0900

    Delete unnecessary code & access modifiers

--- modules/auth/src/main/kotlin/com/kgmyshin/auth/infra/api/json/PublishTokenRequestJson.kt
+++ modules/auth/src/main/kotlin/com/kgmyshin/auth/infra/api/json/PublishTokenRequestJson.kt
@@ -4,9 +4,9 @@ import com.google.gson.annotations.SerializedName
 import com.kgmyshin.auth.BuildConfig
 
 internal data class PublishTokenRequestJson(
-        @SerializedName("code") private val code: String,
-        @SerializedName("client_id") private val clientId: String = BuildConfig.ANNICT_CLIENT_ID,
-        @SerializedName("client_secret") private val clientSecret: String = BuildConfig.ANNICT_CLIENT_SECRET,
-        @SerializedName("grant_type") private val grantType: String = "authorization_code",
-        @SerializedName("redirect_uri") private val redirectUri: String = "urn:ietf:wg:oauth:2.0:oob"
+        @SerializedName("code") val code: String,
+        @SerializedName("client_id") val clientId: String = BuildConfig.ANNICT_CLIENT_ID,
+        @SerializedName("client_secret") val clientSecret: String = BuildConfig.ANNICT_CLIENT_SECRET,
+        @SerializedName("grant_type") val grantType: String = "authorization_code",
+        @SerializedName("redirect_uri") val redirectUri: String = "urn:ietf:wg:oauth:2.0:oob"
 )
