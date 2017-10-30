4387eeb0bdc17a72846c86b7d408db42529881eb
kgmyshin <kgmyshin82@gmail.com>
2017-10-30 19:58:45 +0900

    generate test data factory

---
+++ modules/work-dictionary/src/test/java/com/kgmyshin/workDictionary/infra/api/json/EpisodeJsonFactory.kt
@@ -0,0 +1,20 @@
+package com.kgmyshin.workDictionary.infra.api.json
+
+import com.kgmyshin.random.RandomHelper
+import kotlin.Int
+import kotlin.String
+
+internal object EpisodeJsonFactory {
+  fun create(
+      id: String = RandomHelper.randomString(),
+      number: Int = RandomHelper.randomInt(),
+      numberText: String = RandomHelper.randomString(),
+      sortNumber: Int = RandomHelper.randomInt(),
+      title: String = RandomHelper.randomString(),
+      recordsCount: Int = RandomHelper.randomInt(),
+      recordCommentsCount: Int = RandomHelper.randomInt(),
+      workJson: com.kgmyshin.workDictionary.infra.api.json.WorkJson = com.kgmyshin.workDictionary.infra.api.json.WorkJsonFactory.create(),
+      prevEpisodeJson: com.kgmyshin.workDictionary.infra.api.json.PrevEpisodeJson = com.kgmyshin.workDictionary.infra.api.json.PrevEpisodeJsonFactory.create(),
+      nextEpisodeJson: com.kgmyshin.workDictionary.infra.api.json.NextEpisodeJson = com.kgmyshin.workDictionary.infra.api.json.NextEpisodeJsonFactory.create()
+  ): EpisodeJson = com.kgmyshin.workDictionary.infra.api.json.EpisodeJson(id, number, numberText, sortNumber, title, recordsCount, recordCommentsCount, workJson, prevEpisodeJson, nextEpisodeJson)
+}
