37d499d7a62f1ba89ebc8afe31497b0c81d67e88
kgmyshin <kgmyshin82@gmail.com>
2017-11-05 22:17:54 +0900

    Remove `internal`, because these are used by other module

--- modules/work-dictionary/src/main/kotlin/com/kgmyshin/workDictionary/infra/api/json/FacebookImageJson.kt
+++ modules/work-dictionary/src/main/kotlin/com/kgmyshin/workDictionary/infra/api/json/FacebookImageJson.kt
@@ -2,6 +2,6 @@ package com.kgmyshin.workDictionary.infra.api.json
 
 import com.google.gson.annotations.SerializedName
 
-internal data class FacebookImageJson(
+data class FacebookImageJson(
         @SerializedName("og_image_url") val ogImageUrl: String
 )
