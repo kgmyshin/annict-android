5b6747e4a9d7280f5f49a92e5fb16594c3baa814
kgmyshin <kgmyshin82@gmail.com>
2017-11-06 23:44:47 +0900

    add record domain models

---
+++ modules/record/src/main/kotlin/com/kgmyshin/record/domain/episode/record/RatingState.kt
@@ -0,0 +1,8 @@
+package com.kgmyshin.record.domain.episode.record
+
+internal enum class RatingState(val rawValue: String) {
+    Bad("bad"),
+    Average("average"),
+    Good("good"),
+    Great("great")
+}
---
+++ modules/record/src/main/kotlin/com/kgmyshin/record/domain/user/UserId.kt
@@ -0,0 +1,5 @@
+package com.kgmyshin.record.domain.user
+
+import com.kgmyshin.common.dddSupport.Identifier
+
+internal class UserId(value: String) : Identifier<String>(value)
