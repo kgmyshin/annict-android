2ab0a61fae1f0782621f75a7a62ace586eb6f34a
kgmyshin <kgmyshin82@gmail.com>
2017-11-11 17:27:16 +0900

    fix annotation

--- modules/work-dictionary/src/test/java/com/kgmyshin/workDictionary/usecase/impl/SearchWorkListUseCaseImplSpec.kt
+++ modules/work-dictionary/src/test/java/com/kgmyshin/workDictionary/usecase/impl/SearchWorkListUseCaseImplSpec.kt
@@ -8,8 +8,11 @@ import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
 import org.jetbrains.spek.subject.SubjectSpek
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
 import org.mockito.Mockito
 
+@RunWith(JUnitPlatform::class)
 internal class SearchWorkListUseCaseImplSpec : SubjectSpek<SearchWorkListUseCaseImpl>({
 
     val repository = Mockito.mock(WorkRepository::class.java)
