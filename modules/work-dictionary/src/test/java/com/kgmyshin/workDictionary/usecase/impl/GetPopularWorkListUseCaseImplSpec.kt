8590740902e34e3a956a365e7570412ea7426258
kgmyshin <kgmyshin82@gmail.com>
2017-11-11 17:06:52 +0900

    use Spek in usecase-layer

---
+++ modules/work-dictionary/src/test/java/com/kgmyshin/workDictionary/usecase/impl/GetPopularWorkListUseCaseImplSpec.kt
@@ -0,0 +1,40 @@
+package com.kgmyshin.workDictionary.usecase.impl
+
+import com.kgmyshin.workDictionary.domain.DomainHelper
+import com.kgmyshin.workDictionary.domain.work.WorkRepository
+import io.reactivex.Single
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import org.jetbrains.spek.subject.SubjectSpek
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+
+@RunWith(JUnitPlatform::class)
+internal class GetPopularWorkListUseCaseImplSpec : SubjectSpek<GetPopularWorkListUseCaseImpl>({
+
+    val repository = Mockito.mock(WorkRepository::class.java)
+
+    subject {
+        GetPopularWorkListUseCaseImpl(repository)
+    }
+
+    given("WorkRepository.findAllPopular return workList") {
+        val workList = listOf(
+                DomainHelper.work(),
+                DomainHelper.work(),
+                DomainHelper.work()
+        )
+        Mockito.`when`(repository.findAllPopular()).thenReturn(Single.just(workList))
+
+        on("execute") {
+            val single = subject.execute()
+
+            it("should return workList") {
+                single.test().await().assertValue(workList).assertComplete()
+            }
+        }
+    }
+
+})
